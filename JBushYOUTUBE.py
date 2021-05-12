01dcbda1491f4b306e4721e3da8a64ed447708a8
vatanozcelik <58488277+vatanozcelik@users.noreply.github.com>
2021-05-12 14:25:39 +0300

    Update JBushYOUTUBE.py

--- JBushYOUTUBE.py
+++ JBushYOUTUBE.py
@@ -9,7 +9,7 @@ import nltk
 from nltk.corpus import state_union
 from nltk.tokenize import PunktSentenceTokenizer
 
-trainText = "A malapropism also called a malaprop, acyrologia, or Dogberryism is the mistaken use of an incorrect word in place of a word with a similar sound, resulting in a nonsensical, sometimes humorous utterance. An example is the statement by baseball player Yogi Berra, Texas has a lot of electrical votes, rather than electoral votes. Malapropisms often occur as errors in natural speech and are sometimes the subject of media attention, especially when made by politicians or other prominent individuals. Philosopher Donald Davidson has said that malapropisms show the comple process through which the brain translates thoughts into language. Humorous malapropisms are the type that attract the most attention and commentary, but bland malapropisms are common in speech and writing."
+trainText = "A malapropism also called a malaprop, acyrologia, or Dogberryism is the mistaken use of an incorrect word in place of a word with a similar sound, resulting in a nonsensical, sometimes humorous utterance. An example is the statement by baseball player Yogi Berra, Texas has a lot of electrical votes, rather than electoral votes. Malapropisms often occur as errors in natural speech and are sometimes the subject of media attention, especially when made by politicians or other prominent individuals. Philosopher Donald Davidson has said that malapropisms show the comple process through which the brain translates thoughts into language.Humorous malapropisms are the type that attract the most attention and commentary, but bland malapropisms are common in speech and writing."
 
 
 customSentTokenizer = PunktSentenceTokenizer(trainText)
@@ -27,35 +27,3 @@ def processContent():
         print(str(e))
         
 processContent()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
